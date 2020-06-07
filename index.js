1dd32f6a6ffbf8f3e62e80d5a748814f102bd4d4
iyanu27 <iyanuoluwatosindaramola@gmail.com>
2020-06-07 18:38:16 +0100

    I added welcome to a new week

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("welcome to introduction to version control!!!!!!");
 console.log("This is a correction added to the index file");
 console.log("My name is iyanuoluwa Daramola");
+console.log("Welcome to a new week");
